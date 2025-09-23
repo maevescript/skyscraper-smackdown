801bfe9c61487c6b1f55309bb60b07a80bfdb00e
Uziel Barrita <leibarrita@gmail.com>
2025-09-23 17:20:29 -0600

    debris generator time intervals

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import arcade
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from game.systems.debris_generator import DebrisGenerator
 
 # Constants
 WINDOW_WIDTH = 1280
@@ -14,6 +18,8 @@ class GameView(arcade.Window):
 
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
 
+        self.debris_generator = DebrisGenerator()
+
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
         pass
@@ -26,8 +32,11 @@ class GameView(arcade.Window):
         # set to. This ensures that you have a clean slate for drawing each
         # frame of the game.
         self.clear()
+        self.debris_generator.draw()
 
         # Code to draw other things will go here
+    def on_update(self, delta_time: float) -> None:
+        self.debris_generator.update(delta_time)
 
 
 def main():
